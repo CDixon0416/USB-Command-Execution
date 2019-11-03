d850b3a2dbafe4ccf2ac46412baf92e550eb60f8
CDixon0416 <caseyericdixon@gmail.com>
2019-11-03 11:08:58 -0500

    Adding strings of OUI prefix from MAC Address

--- USB Command Execution.py
+++ USB Command Execution.py
@@ -1,7 +1,23 @@
 import os
+import re
 
 # Gather data from basic shell commands
 ipconfig = os.popen('ipconfig /all').read()
 arp = os.popen('arp -a').read()
 hostname = os.popen('hostname').read()
+print(arp)
 
+arpValues = re.findall("..-..-..-..-..-..", arp)
+count = len(arpValues)
+
+arpValues = str(arpValues)
+arpValues = arpValues.replace('-', ':')
+
+startString = 2
+endString = 10
+for i in range(count):
+    tempstr = arpValues[startString:endString]
+    print(tempstr)
+    startString = startString + 21
+    endString = endString + 21
+print(arpValues)
