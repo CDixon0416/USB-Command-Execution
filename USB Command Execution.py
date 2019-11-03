ce405223c610eb92940eb9f5fcddf61f9c64c220
CDixon0416 <caseyericdixon@gmail.com>
2019-11-03 13:39:58 -0500

    Creating list from xml elements

--- USB Command Execution.py
+++ USB Command Execution.py
@@ -1,30 +1,10 @@
 import os
 import re
-import xml
-
-# Gather data from basic shell commands
 import xml.etree.ElementTree
 
-ipconfig = os.popen('ipconfig /all').read()
-arp = os.popen('arp -a').read()
-hostname = os.popen('hostname').read()
-
-# Create the list of MAC addresses and gather the number of results for iterating through
-arpValues = re.findall("..-..-..-..-..-..", arp)
-count = len(arpValues)
-arpValues = str(arpValues)
-# Replace the '-' with a ':' for comparing the OUI values against the xml file
-arpValues = arpValues.replace('-', ':')
-
-# Declare the index's of the OUI portion of the MAC addresses
-startString = 2
-endString = 10
-# Import the xml file that contains the list of known OUIs
-tree = xml.etree.ElementTree.parse('OUI2.xml')
-root = tree.getroot()
-ouiList = []
-
 # Only needed to remove unnecessary elements in original xml file
+# treeinit = xml.etree.ElementTree.parse('OUI.xml')
+# rootinit = tree.getroot()
 # for elem in root.iter():
 #     for child in list(elem):
 #         if child.tag == 'isPrivate':
@@ -42,17 +22,35 @@ ouiList = []
 #
 # tree.write('OUI2.xml')
 
-# for record in root.findall('record'):
-#
-#     oui = record.find('oui').text
-#     manufacturer = record.find('companyName').text
-#     ouiList.append([oui, manufacturer])
-#
-# ouiList = str(ouiList)
-# for i in range(count):
-#     tempstr = arpValues[startString:endString]
-#     print(tempstr)
-#     match = re.findall(tempstr, ouiList)
-#     print(match)
-#     startString = startString + 21
-#     endString = endString + 21
+# Gather data from basic shell commands
+ipconfig = os.popen('ipconfig /all').read()
+arp = os.popen('arp -a').read()
+hostname = os.popen('hostname').read()
+
+# Create the list of MAC addresses and gather the number of results for iterating through
+arpValues = re.findall("..-..-..-..-..-..", arp)
+count = len(arpValues)
+arpValues = str(arpValues)
+
+# Replace the '-' with a ':' for comparing the OUI values against the xml file
+arpValues = arpValues.replace('-', ':')
+
+# Declare the index's of the OUI portion of the MAC addresses
+startString = 2
+endString = 10
+
+# Import the xml file that contains the list of known OUIs
+tree = xml.etree.ElementTree.parse('OUI2.xml')
+root = tree.getroot()
+
+for i in range(count):
+    oui = arpValues[startString:endString]
+    oui = oui.upper()
+    x = 0
+    for record in root:
+        rootHolder = root[x].findtext("oui")
+        if rootHolder == oui:
+            print(root[x].findtext("oui"), root[x].findtext("companyName"))
+        x = x + 1
+    startString = startString + 21
+    endString = endString + 21
